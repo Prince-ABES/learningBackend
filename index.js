b28cec50052a470ba73a74305a61f533ca5ade6f
Prince-ABES <prk988110@gmail.com>
2026-06-09 10:54:25 +0530

    add .env file to .gitignore and updageindependency

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
@@ -61,5 +62,5 @@ app.delete("/teas/:id", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}/`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
